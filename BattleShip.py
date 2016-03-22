1309ca1318fb2283923e38405ee0ff2b0cc30723
Florian Sperber <florian.sperber@mail.utoronto.ca>
2016-03-22 16:19:11 -0400

    Update BattleShip.py
    adapting for python2.7

--- BattleShip.py
+++ BattleShip.py
@@ -10,7 +10,7 @@ while 1 == 1:
         for row in board:
             print ("~~".join(row))
 
-    print ("Let's play Battleship!")
+    print "Let's play Battleship!"
     print_board(board)
 
     def random_row(board):
@@ -19,6 +19,9 @@ while 1 == 1:
     def random_col(board):
         return randint(0, len(board[0]) - 1)
 
+    def ship_pos (board)
+        return random_row +
+
     ship_row = random_row(board)
     ship_col = random_col(board)
     print (ship_row)
@@ -26,28 +29,27 @@ while 1 == 1:
 
     
     for turn in range(4):
-        print ("Turn", turn + 1)
-        guess_row = int(input("Guess Row:"))
-        guess_col = int(input("Guess Col:"))
+        print "Turn", turn + 1
+        guess_row = int(input("Guess Row:")) - 1
+        guess_col = int(input("Guess Col:")) - 1
 
 
         if guess_row == ship_row and guess_col == ship_col: 
-            print ("Congratulations! You sunk my battleship!")
+            print "Congratulations! You sunk my battleship!"
             break
             restart = input ('press any key to restart')
             
         
         else:
             if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-                print ("Oops, that's not even in the ocean.")
+                print "Oops, that's not even in the ocean."
             elif(board[guess_row][guess_col] == "X"):
-                print ("You guessed that one already.")
+                print "You guessed that one already."
             else:
-                print ("You missed my battleship!")
+                print "You missed my battleship!"
                 board[guess_row][guess_col] = "X"
             if turn == 3:
-                print ("Game Over")
+                print "Game Over"
                 break
         
         print_board(board)
-
